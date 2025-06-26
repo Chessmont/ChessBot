e619866d58f3323c373612f3f70e2a8434d10251
Djahmo <clayton.girard@icloud.com>
2025-06-26 05:22:29 +0200

    remove perm in command

--- api/db/commands.js
+++ api/db/commands.js
@@ -5,35 +5,33 @@ const initDBcommands = async (dbFromInit, client) => {
 
     if(!client) {
         await db.query(/*sql*/`DROP TABLE IF EXISTS commands`);
-        
+
         await db.query(/*sql*/`
             CREATE TABLE IF NOT EXISTS commands (
                 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
                 name VARCHAR(50) NOT NULL,
                 info TEXT NOT NULL,
                 description TEXT NOT NULL,
-                permsLevel INT UNSIGNED NOT NULL,
                 createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                FOREIGN KEY (permsLevel) REFERENCES permissions(id)
+                updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             )
         `);
     }
 };
 
-const addCommand = async (commandInfo, permsLevel) => {
+const addCommand = async (commandInfo) => {
     const [command] = await db.query(/*sql*/`SELECT * FROM commands WHERE name = ?`, [commandInfo.commandName]);
     if (!command.length) {
         await db.query(/*sql*/`
-            INSERT INTO commands (name, info, description, permsLevel)
-            VALUES (?, ?, ?, ?)
-        `, [commandInfo.commandName, commandInfo.commandInfo, commandInfo.commandDescription, permsLevel]);
+            INSERT INTO commands (name, info, description)
+            VALUES (?, ?, ?)
+        `, [commandInfo.commandName, commandInfo.commandInfo, commandInfo.commandDescription]);
     } else {
         await db.query(/*sql*/`
             UPDATE commands
-            SET info = ?, description = ?, permsLevel = ?
+            SET info = ?, description = ?
             WHERE name = ?
-        `, [commandInfo.commandInfo, commandInfo.commandDescription, permsLevel, commandInfo.commandName]);
+        `, [commandInfo.commandInfo, commandInfo.commandDescription, commandInfo.commandName]);
     }
 };
 
